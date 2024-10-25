afc3c509b5a180cd76ed037f6b5b67df00cc16ff
sai184 <sainagarjuna184@gmail.com>
2024-10-25 09:28:15 -0500

    v5

---
+++ main.tf
@@ -0,0 +1,19 @@
+module "vpc" {
+  source="./modules/VPC"
+variable "env" {}
+variable "vpc_cidr" {}
+variable "public_subnets" {}
+variable "private_subnets" {}
+variable "azs" {}
+variable "account_id" {}
+variable "default_vpc_id" {}
+variable "default_vpc_id_cidr" {}
+variable "default_route_table_id" {}
+#variable "vpc_id" {}
+variable "bastion_node_cidr" {}
+
+
+  
+
+  
+}
---
+++ var.tf
@@ -0,0 +1,10 @@
+variable "vpc_cidr" {}
+variable "env" {}
+variable "public_subnets" {}
+variable "azs" {}
+variable "private_subnets" {}
+variable "account_id" {}
+variable "default_vpc_id_cidr"{}
+variable "default_route_table_id"{}  
+
+
